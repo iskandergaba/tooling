4df3a08e59dc10cef4c07c19cbd2e5ce1cb9df1a
Iskander <iskander@hey.com>
2023-03-29 02:57:23 +0200

    Removed extra line

--- git/git.sh
+++ git/git.sh
@@ -2,4 +2,3 @@
 
 # git uncommit: Remove the  last commit (or optionally the last n commits) in the current branch
 git config --global alias.uncommit '!f() { git reset HEAD~"${1:-1}" --hard; }; f'
-
