e191a37589aaed1b75d4c6d97a874b943452462f
Iskander Gaba <iskander@hey.com>
2024-05-26 16:58:46 +0200

    Set the default editor to nano

--- git/git.sh
+++ git/git.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Set the default editor to nano
+git config --global core.editor "nano"
+
 # git uncommit: Remove the last n commits (n=1 if not specified) in the current branch
 git config --global alias.uncommit '!f() { git reset HEAD~"${1:-1}" --hard; }; f'
